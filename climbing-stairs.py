ef4aa9e0df8fe71005c4d3f80b4996b90c9fe9b4
tn259 <tjnewman2013@gmail.com>
2021-03-08 08:49:07 +0000

    Update climbing-stairs.py

--- climbing-stairs.py
+++ climbing-stairs.py
@@ -14,28 +14,12 @@ class Solution:
         table[n] = ways
       return table[n]
         
-    def tabulated(self, n: int) -> int:
-      #table = [0] * 3
-      #for j in range(3):
-        #table[j] = [0] * (n+1)
+    #def tabulated(self, n: int) -> int:
+      #table = [][]
       
-      #for j in range(3):
-        #for i in range(n+1):
-          #if j==0 or i==0:
-            #table[j][i] = 1
-          #elif j==1:
-            #table[j][i] = 1
-          #else:
-            #table[j][i] = table[j][i-1] + table[j][i-2]
-      table = [1] * (n+1)
-      for i in range(2, n+1):
-        table[i] = table[i-1] + table[i-2]
-      return table[n]
-            
       
       
     def climbStairs(self, n: int) -> int:
-      #table = {}
-      #return self.climbStairsRec(n, table)
-      return self.tabulated(n)
+      table = {}
+      return self.climbStairsRec(n, table)
         
