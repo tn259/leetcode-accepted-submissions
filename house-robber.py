feb3407f447b337276c82cd4475b780cb16dace4
tn259 <tjnewman2013@gmail.com>
2021-03-10 13:17:06 +0000

    Update house-robber.py

--- house-robber.py
+++ house-robber.py
@@ -1,26 +1,30 @@
 class Solution:
-    def rob(self, nums: List[int]) -> int:
-      
-      if len(nums) == 0:
+    def robrec(self, nums: List[int], i: int, memoized: List[int]) -> int:
+      if i >= len(nums):
         return 0
       
-      if len(nums) == 1:
-        return nums[0]
-    
+      if i == len(nums)-1 or i == len(nums)-2:
+        return nums[i]
+      
+      if memoized[i] != -1:
+        return memoized[i]
+      
+      greatest = 0
+      for j in range(i+2, len(nums)):
+        robbed = nums[i] + self.robrec(nums, j, memoized)
+        if robbed > greatest:
+          greatest = robbed
+      
+      memoized[i] = greatest
+      return greatest
+  
+    def rob(self, nums: List[int]) -> int:
+      memoized = [-1] * len(nums)
+      r0 = self.robrec(nums, 0, memoized)
+      r1 = self.robrec(nums, 1, memoized)
+      if r0 > r1:
+        return r0
+      return r1
+      
 
-      table = [0] * len(nums)
-      
-      table[len(nums)-1] = nums[len(nums)-1]
-      table[len(nums)-2] = nums[len(nums)-2]
-      
-      for i in range(len(nums)-3,-1,-1):
-        table[i] = table[i+2]+nums[i]
-        if table[i+2]>table[i+1]:
-          table[i+1] = table[i+2]
-          
-      if table[0] > table[1]:
-        return table[0]
-      
-      return table[1]
 
-        
